f0d2b4fe8000f435e1fa0784d2000e32fe7bfd5b
danjocutler <danjocutler@gmail.com>
2014-11-03 15:23:02 +0000

    Added update functionality

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -39,11 +39,28 @@ describe 'creating restaurants' do
 			@kfc = Restaurant.create(name:'KFC')
 		end
 
-		xit 'lets a user view a restaurant' do
+		it 'lets a user view a restaurant' do
 			visit '/restaurants'
-			click_link 'KFC'
 			expect(page).to have_content 'KFC'
+			click_link 'KFC'
 			expect(current_path).to eq "/restaurants/#{@kfc.id}"
 		end
 	end
+
+	context 'editing restaurants' do
+
+  before do 
+    Restaurant.create(name:'KFC') 
+  end
+
+  it 'lets a user edit a restaurant' do
+   visit '/restaurants'
+   click_link 'Edit KFC'
+   fill_in 'Name', with: 'Kentucky Fried Chicken'
+   click_button 'Update Restaurant'
+   expect(page).to have_content 'Kentucky Fried Chicken'
+   expect(current_path).to eq '/restaurants'
+  end
+
+end
 end
