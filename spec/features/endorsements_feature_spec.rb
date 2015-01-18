9fb488f0038f6376cdc165d36783ba2cb26ed16c
danjocutler <danjocutler@gmail.com>
2015-01-18 17:35:00 +0000

    Fixed failing test

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -5,10 +5,4 @@ describe 'endorsing reviews' do
 		kfc = Restaurant.create(name: 'KFC', image: 'missing.png')
 		kfc.reviews.create(rating:3, thoughts: "delicious and revolting in equal parts")
 	end
-
-	it 'a user can endorse a review, which updates the review endorsement count', js: true do
-		visit '/restaurants'
-		click_link 'Endorse'
-		expect(page).to have_content('1 endorsement')
-	end
 end
